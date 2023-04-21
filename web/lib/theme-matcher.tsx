c4a1451aaa4150f95c2afdbde7a9371d43701119
Rodrigo Pombo <pombopombopombo@gmail.com>
2023-04-21 10:39:42 +0000

    Fix rule tiebreaker

--- web/lib/theme-matcher.tsx
+++ web/lib/theme-matcher.tsx
@@ -1,24 +1,23 @@
 // scopes starts with the root scope and ends with the token scope
 export function getBestMatchRule(theme: any, scopes: string[]) {
-  const { tokenColors: rules } = theme || [];
+  const { tokenColors: rules } = theme || []
 
-  let bestScore = 0;
-  let bestRule = null;
+  let bestScore = 0
+  let bestRule = null
   for (let i = 0; i < rules.length; i++) {
-    const rule = rules[i];
-    const score = scopesMatchesRule(scopes, rule);
-    if (score > bestScore) {
-      bestScore = score;
-      bestRule = rule;
-      // console.log({ rule, score, scopes: scopesDescending });
+    const rule = rules[i]
+    const score = scopesMatchesRule(scopes, rule)
+    if (score >= bestScore) {
+      bestScore = score
+      bestRule = rule
     }
   }
 
   if (!bestRule) {
     // return global rule
-    return rules.find((rule) => !rule.scope);
+    return rules.find((rule) => !rule.scope)
   }
-  return bestRule;
+  return bestRule
 }
 
 function scopesMatchesRule(scopes, rule) {
@@ -27,57 +26,57 @@ function scopesMatchesRule(scopes, rule) {
       ? rule.scope.split(/,/).map((scope) => scope.trim())
       : Array.isArray(rule.scope)
       ? rule.scope
-      : [];
+      : []
   if (selectors.length === 0) {
-    return 0;
+    return 0
   }
 
   const scores = selectors.map((selector) =>
     scopesMatchesSelector(scopes, selector)
-  );
-  return Math.max(...scores);
+  )
+  return Math.max(...scores)
 }
 
 // example:
 // scopes: ["source.js", "meta.function.js", "variable.parameter.js"]
 // selector: "meta.function variable"
 function scopesMatchesSelector(scopes, selector) {
-  const subSelectors = selector.split(/ /); // ["meta.function", "variable"]
-  const scores = scopes.map((scope) => 0);
-  let scopeDepth = 0;
+  const subSelectors = selector.split(/ /) // ["meta.function", "variable"]
+  const scores = scopes.map((scope) => 0)
+  let scopeDepth = 0
 
   // TODO handle Excluding Elements "source.ruby string - string source"
   // https://macromates.com/manual/en/scope_selectors
 
   for (let i = 0; i < subSelectors.length; i++) {
-    const subSelector = subSelectors[i];
+    const subSelector = subSelectors[i]
 
-    let score = 0;
+    let score = 0
     for (
       let scopeIndex = scopeDepth;
       scopeIndex < scopes.length;
       scopeIndex++
     ) {
-      const scope = scopes[scopeIndex];
-      const scopeScore = scopeMatchesSubSelector(scope, subSelector);
+      const scope = scopes[scopeIndex]
+      const scopeScore = scopeMatchesSubSelector(scope, subSelector)
       if (scopeScore > 0) {
-        score = scopeScore;
-        scopeDepth = scopeIndex + 1;
-        scores[scopeIndex] = score;
-        break;
+        score = scopeScore
+        scopeDepth = scopeIndex + 1
+        scores[scopeIndex] = score
+        break
       }
     }
 
     if (score === 0) {
-      return 0;
+      return 0
     }
   }
 
-  let score = 0;
+  let score = 0
   for (let i = 0; i < scores.length; i++) {
-    score += scores[i] * Math.pow(10, i);
+    score += scores[i] * Math.pow(10, i)
   }
-  return score;
+  return score
 }
 // example:
 // scopes: "meta.function.js"
@@ -87,9 +86,9 @@ function scopesMatchesSelector(scopes, selector) {
 // selector: "meta"
 // returns 1
 function scopeMatchesSubSelector(scope, selector) {
-  const selectorPrefix = selector + ".";
+  const selectorPrefix = selector + "."
   if (selector === scope || scope.startsWith(selectorPrefix)) {
-    return selector.split(".").length;
+    return selector.split(".").length
   }
-  return 0;
+  return 0
 }
